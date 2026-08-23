e6e44ff5361b6be21e2edda6bfb79f6a03b99751
Aditya Singh <Adityasingh60908@gmail.com>
2026-08-24 02:51:43 +0530

    Backend traverse fixed

--- url-backend/src/main/java/org/example/controller/UrlController.java
+++ url-backend/src/main/java/org/example/controller/UrlController.java
@@ -14,7 +14,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.view.RedirectView;
-
+import org.springframework.http.ResponseEntity;
 import java.util.List;
 import java.util.Map;
 
